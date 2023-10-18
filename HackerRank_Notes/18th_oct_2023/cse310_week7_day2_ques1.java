5eaf7a4eda7e556f9e6cc4e5c2f6a92df671f577
krishna1809 <kancharlakrishnakanth523@gmail.com>
2023-10-18 18:34:32 +0530

    Added on 18th_Oct_2023

---
+++ HackerRank_Notes/18th_oct_2023/cse310_week7_day2_ques1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan=new Scanner(System.in);
+        String s1=scan.nextLine();
+        String s2=s1.toLowerCase();
+        System.out.println(s2.replace(" ","$"));
+    }
+}
